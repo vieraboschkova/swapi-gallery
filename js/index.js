cc02d5b742601690273ca05f4a0219cab1e57b89
VieraBoschkova <vieraboschkova@gmail.com>
2021-05-14 17:47:26 +0200

    feat: display modal with fetched details

--- js/index.js
+++ js/index.js
@@ -1,12 +1,10 @@
-import * as api from './apiCall';
-import 'regenerator-runtime/runtime';
-import CharacterCard from './card';
+import * as api from "./apiCall";
+import "regenerator-runtime/runtime";
+import CharacterCard from "./card";
 
-const gallery = document.getElementById('galleryRoot');
+const gallery = document.getElementById("galleryRoot");
 const items = api.getCharacters();
-const cards = items.map(({
-  name, gender, eyeColor, birthYear,
-}) => {
+const cards = items.map(({ name, gender, eyeColor, birthYear }) => {
   const card = new CharacterCard(name, gender, eyeColor, birthYear);
   return card.component;
 });
